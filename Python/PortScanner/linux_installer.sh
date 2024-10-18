13b65d6e837f633e661d42577f8832919c04535a
Vitaliy Markovskiy <vitmark06@gmail.com>
2024-10-18 16:05:58 +0300

    Delete Python/PortScanner/linux_installer.sh

--- Python/PortScanner/linux_installer.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-python3 -m venv env
-source env/bin/activate
-pip3 install pyqt5
-pyinstaller --onefile -w 'main.py'
-cp dist/main ./portscanner
-rm -rf build dist env main.spec
-./portscanner
