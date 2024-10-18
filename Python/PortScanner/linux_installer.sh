70076cefcd931a4eed21c44bebe4e69b7013e59a
Vitaliy Markovskiy <vitmark06@gmail.com>
2024-10-18 15:36:59 +0300

    Add files via upload
    Added installer for linux

---
+++ Python/PortScanner/linux_installer.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+python3 -m venv env
+source env/bin/activate
+pip3 install pyqt5
+pyinstaller --onefile -w 'main.py'
+cp dist/main ./portscanner
+rm -rf build dist env main.spec
+./portscanner
