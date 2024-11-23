89557860f3656fb09b16a0c9b4d6aa48b902f9e7
WhiteCarVM <vitmark06@gmail.com>
2024-11-23 23:21:03 +0300

    Create a new file for netcat class and update help menu

--- Python/MyNetCat/main.py
+++ Python/MyNetCat/main.py
@@ -2,6 +2,7 @@ import argparse
 import shlex
 import socket
 import subprocess
+import sys
 import textwrap
 
 def parser():
@@ -14,18 +15,19 @@ def parser():
     description='This is my netcat',
     formatter_class=argparse.RawDescriptionHelpFormatter,
     epilog=textwrap.dedent('''Examples:
+            python3 mynetcat.py -t 8.8.8.8 -p 4444 # connect to target
             python3 mynetcat.py -t 8.8.8.8 -p 4444 -l #connect to target and listen
             python3 mynetcat.py -t 8.8.8.8 -p 4444 -l -c # connect to target and use command shell
             python3 mynetcat.py -t 8.8.8.8 -p 4444 -l -u=test.txt #connect to target and upload file "test.txt"
-            python3 mynetcat.py -t 8.8.8.8 -p 4444 -e="cat /etc/passwd" # connect to target and execute command "cat /etc/passwd"
+            python3 mynetcat.py -t 8.8.8.8 -p 4444 -l -e="cat /etc/passwd" # connect to target and execute command "cat /etc/passwd"
     '''))
 
     
     args.add_argument('-c', '--comand', action='store_true', help='use command shell')
     args.add_argument('-e', '--execute', help='execute specified command')
     args.add_argument('-l', '--listen', action='store_true', help='listen')
-    args.add_argument('-p', '--port', type=int, default=10000, help="target port to connection")
-    args.add_argument('-t', '--target', help='target IP address to connection')
+    args.add_argument('-p', '--port', type=int, default=10000, required=True, help="target port to connection")
+    args.add_argument('-t', '--target', required=True, help='target IP address to connection')
     args.add_argument('-u', '--upload', help='upload file to target')
 
     return args.parse_args()
@@ -44,3 +46,7 @@ def execute(command):
 
 if __name__ == "__main__":
     args = parser()
+    if args.listen:
+        buffer = ""
+    else:
+        buffer = sys.stdin.read()
