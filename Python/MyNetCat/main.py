e1f286f10f3c47d66f73e7c5c946b9e12ef078dc
WhiteCarVM <vitmark06@gmail.com>
2024-11-24 09:53:20 +0300

    Update menu

--- Python/MyNetCat/main.py
+++ Python/MyNetCat/main.py
@@ -1,5 +1,4 @@
 import argparse
-import shlex
 import textwrap
 
 import mynetcat
@@ -10,10 +9,10 @@ def parser():
     """
     
     args = argparse.ArgumentParser(
-    prog="MyNetCat",
-    description='This is my netcat',
-    formatter_class=argparse.RawDescriptionHelpFormatter,
-    epilog=textwrap.dedent('''Examples:
+        prog="MyNetCat",
+        description='This is my netcat',
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        epilog=textwrap.dedent('''Examples:
             python3 mynetcat.py -t 8.8.8.8 -p 4444 # connect to target
             python3 mynetcat.py -t 8.8.8.8 -p 4444 -l #connect to target and listen
             python3 mynetcat.py -t 8.8.8.8 -p 4444 -l -c # connect to target and use command shell
@@ -24,9 +23,9 @@ def parser():
     
     args.add_argument('-c', '--command', action='store_true', help='use command shell')
     args.add_argument('-e', '--execute', type=str, help='execute specified command')
-    args.add_argument('-l', '--listen', action='store_true', help='listen')
+    args.add_argument('-l', '--listen', action='store_true', help='listen mode')
     args.add_argument('-p', '--port', type=int, default=10000, required=True, help="target port to connection")
-    args.add_argument('-t', '--target', type=str, required=True, help='target IP address to connection')
+    args.add_argument('-t', '--target', type=str, default="0.0.0.0", required=True, help='target IP address to connection')
     args.add_argument('-u', '--upload', help='upload file to target')
 
     return args.parse_args()
