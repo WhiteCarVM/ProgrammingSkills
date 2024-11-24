fc4171b9f4f0f5013adcb85bf6d0ddf31b309d85
WhiteCarVM <vitmark06@gmail.com>
2024-11-24 10:03:50 +0300

    Update output and correct sender function

--- Python/MyNetCat/mynetcat.py
+++ Python/MyNetCat/mynetcat.py
@@ -2,6 +2,7 @@ import shlex
 import socket
 import subprocess
 import sys
+import time
 import threading
 import argparse
 
@@ -10,6 +11,7 @@ class MyNetCat:
         """
             This function initializes some parameters
         """
+
         self.args = args
         self.buffer = buffer
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -19,92 +21,145 @@ class MyNetCat:
         """
             This function runs MyNetCat 
         """
-        if self.args.listen:
-            self.listen()
-        else:
-            self.send()
+
+        try:
+            if self.args.listen:
+                self.listen()
+            else:
+                self.send()
+        except KeyboardInterrupt:
+            time.sleep(1)
+            print("\nExiting...")
+            time.sleep(1)
+            self.socket.close()
+            sys.exit()
 
     def execute(self, command):
         """
             This function is used for executing commands on target
         """
+
         command = command.strip()
-        response = subprocess.check_output(shlex.split(command), stderr=subprocess.STDOUT)
-        return response.decode()
+
+        try:
+            response = subprocess.check_output(shlex.split(command), stderr=subprocess.STDOUT)
+            return response.decode()
+        except subprocess.CalledProcessError as ex:
+            return f"Command failed: {ex.output.decode()}"
 
     def handle(self, client_socket):
         """
-            This function creates tasks for arguments
+        This function creates tasks for arguments.
         """
+
         if self.args.execute:
             response = self.execute(self.args.execute)
-            client_socket.send(response.encode())
+            try:
+                client_socket.send(response.encode())
+            except Exception as e:
+                print(f"Error sending response: {e}")
+            client_socket.close()
+        
         elif self.args.upload:
             file_buffer = b''
             while True:
-                data = client_socket.recv(4096)
-                if data:
-                    file_buffer += data
-                else:
-                    break
-            with open(self.args.upload, 'wb') as file:
-                file.write(file_buffer)
-            message = f"File {self.args.upload} saved"
-            client_socket.send(message.encode())
-        elif self.args.command:
-            cmd_buffer = b''
-            while True:
                 try:
-                    client_socket.send(b"user:> ")
-                    while b'\n' not in cmd_buffer.decode():
-                        cmd_buffer += client_socket.recv(64)
-                    response = self.execute(cmd_buffer.decode())
-                    if response:
-                        client_socket.send(response.encode())
+                    data = client_socket.recv(4096)
+                    if not data:
+                        break
+                    file_buffer += data
+                except Exception as e:
+                    print(f"Error receiving data: {e}")
+                    break
+        
+            if file_buffer:
+                with open(self.args.upload, 'wb') as file:
+                    file.write(file_buffer)
+                message = f"File {self.args.upload} saved successfully."
+                try:
+                    client_socket.send(message.encode())
+                except Exception as e:
+                    print(f"Error sending response: {e}")
+            client_socket.close()
+    
+        elif self.args.command:
+            try:
+                while True:
+                    client_socket.send(b"user: @> ")
                     cmd_buffer = b''
-                except Exception as ex:
-                    print("Server killed:", ex)
-                    self.socket.close()
-                    sys.exit()
+
+                    while b'\n' not in cmd_buffer:
+                        try:
+                            data = client_socket.recv(1024)
+                            if not data:
+                                print("Client disconnected.")
+                                return
+                            cmd_buffer += data
+                        except Exception as e:
+                            print(f"Error receiving command: {e}")
+                            return
+                
+                    if cmd_buffer:
+                        response = self.execute(cmd_buffer.decode().strip())
+                        if response:
+                            try:
+                                client_socket.send(response.encode())
+                            except BrokenPipeError:
+                                print("Client disconnected while sending response.")
+                            except Exception as e:
+                                print(f"Error sending response: {e}")
+            except Exception as ex:
+                print("Error occurred:", ex)
+            finally:
+                client_socket.close()
+
 
     def listen(self):
         """
             This function describes listener for incoming clients
         """
+
         self.socket.bind((self.args.target, self.args.port))
         self.socket.listen(5)
         print(f"Listening on {self.args.target}:{self.args.port}")
 
         while True:
             client_socket, address = self.socket.accept()
-            print(f"Connection from {address} established.")
+            print(f"Connection from {address[0]}:{address[1]} established.")
             client_thread = threading.Thread(target=self.handle, args=(client_socket,))
             client_thread.start()
 
     def send(self):
         """
-            This function describes sender to MyNetCat
+        This function describes sender to MyNetCat
         """
-        self.socket.connect((self.args.target, self.args.port))
 
-        if self.buffer:
-            self.socket.send(self.buffer)
-
-        recv_length = 1
-        response = ""
         try:
-            while recv_length:
-                data = self.socket.recv(4096)
-                recv_length = len(data)
-                response += data.decode()
-                if recv_length < 4096:
+            self.socket.connect((self.args.target, self.args.port))
+
+            if self.buffer:
+                self.socket.send(self.buffer)
+
+            while True:
+                recv_length = 1
+                response = ""
+                while recv_length:
+                    data = self.socket.recv(4096)
+                    recv_length = len(data)
+                    response += data.decode()
+                    if recv_length < 4096:
+                        break
+            
+                if response:
+                    print(response)
+
+                try:
+                    buffer = input("> ") + "\n"
+                    self.socket.send(buffer.encode())
+                except EOFError:
+                    print("Exiting.")
                     break
-                
-            if response:
-                print(response)
-                buffer = input("> ") + "\n"
-                self.socket.send(buffer.encode())
         except KeyboardInterrupt:
-            print("User terminated\n")
+            print("\nUser terminated")
+        finally:
             self.socket.close()
-            sys.exit()
