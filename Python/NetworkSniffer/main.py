29b0e3c26e24c3b567be5b164e589068aa5bf299
WhiteCar <whitecar@whitecar>
2025-01-03 22:01:56 +0300

    Added simple sniffer for one IP and one port

--- Python/NetworkSniffer/main.py
+++ Python/NetworkSniffer/main.py
@@ -1,3 +1,33 @@
+import socket
 import os
 
-print(os.name)
+OS = os.name
+# добавить фильтрацию входных данных
+HOST = input("Enter target host\n> ")
+
+def main():
+    """
+        Функция отвечает за создание сокета и получение ответа
+    """
+
+    if OS == 'nt':
+        socket_protocol = socket.IPPROTO_IP
+    else:
+        socket_protocol = socket.IPPROTO_ICMP
+
+    sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocol)
+    sniffer.bind((HOST, 0))
+    sniffer.setsockopt(socket.IPPROTO_ICMP, socket.IP_HDRINCL, 1)
+
+    if OS == 'nt':
+        sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
+    
+    print(sniffer.recvfrom(65565))
+
+    if OS == 'nt':
+        sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
+
+    sniffer.close()
+
+if __name__ == "__main__":
+    main()
