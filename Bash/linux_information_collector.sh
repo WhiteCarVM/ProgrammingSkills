0aee5838a9209970b14f5aa881b4128afaa36675
WhiteCarVM <vitmark06@gmail.com>
2025-01-09 22:01:46 +0300

    Added new part: information about network

--- Bash/linux_information_collector.sh
+++ Bash/linux_information_collector.sh
@@ -3,7 +3,7 @@
 echo "Скрипт для сбора информации об системе. Расчитан на использование против дистрибутивов linux."
 echo "	Скрипт собирает следующую информацию:"
 echo "1. Информация о системе: ядро, архитектура, процессор, жесткий диск, запущенные процессы"
-echo "2. Информация о сети: сетевые интерфейсы, развернутые службы, открытые порты"
+echo "2. Информация о сети: сетевые интерфейсы, таблица маршрутизации, arp-таблица, сетевые подключения, открытые порты  и развернутые сервисы."
 echo "3. Пользователи и группы: история действий, права и файлы пользователей, ключи и пароли"
 echo "4. Доступное ПО: полезное для пивотинга и эскалации привелегий программное обеспечение"
 echo "5. Файлы и резервные копии: файлы, содержащие полезную информацию, резервные копии недоступных для чтениы файлов"
@@ -15,22 +15,18 @@ echo "10.Доступные уязвимости: уязвимости, кото
 
 echo "ЗАМЕЧАНИЕ:"
 echo -e "Информация, связанная с root, помечается с помощью \033[40m \033[31m такой комбинации \033[0m"
-echo -e "Информация, связанная с root, помечается с помощью \033[40m \033[34m такой комбинации \033[0m"
+echo -e "Информация, связанная с текущим пользователем, помечается с помощью \033[40m \033[34m такой комбинации \033[0m"
 echo "Создатель: WhiteCar"
-echo
 
-echo "-------------------------------------------------------------------------------------------------------------------"
+echo -e "\n-------------------------------------------------------------------------------------------------------------------"
 echo "Часть 1: Информация о системе"
-echo
-echo "1) Ядро: $(uname -s)"
+echo -e "\n1) Ядро: $(uname -s)"
 echo "Релиз: $(uname -r)"
 echo "Версия: $(uname -v)"
 echo "Архитектура: $(uname -m)"
 echo "Операционная система: $(uname -o)"
 
-echo
-
-echo "2) Процессор"
+echo -e "\n2) Процессор"
 echo "Производитель: $(lscpu | grep 'Vendor ID:' | cut -d : -f 2 | awk '{$1=$1; print}')"
 echo "Модель: $(lscpu | grep 'Model name:' | cut -d : -f 2 | awk '{$1=$1; print}')"
 echo "Семейство: $(lscpu | grep 'CPU family:' | cut -d : -f 2 | awk '{$1=$1; print}')"
@@ -40,19 +36,62 @@ echo "Гиперпоточность: $(lscpu | grep 'Thread(s) per core:' | cut
 echo "Минимальная частота: $(lscpu | grep 'CPU min MHz:' | cut -d : -f 2 | awk '{$1=$1; print}') MHz"
 echo "Максимальная частота: $(lscpu | grep 'CPU max MHz:' | cut -d : -f 2 | awk '{$1=$1; print}') MHz"
 
-echo
-
-echo "3) Жесткие диски"
-echo "Диски и их размеры:" 
+echo -e "\n3) Жесткие диски"
+echo "Диски и их размеры:"
 disks=$(lsblk -x TYPE | grep disk | cut -d ' ' -f 1)
 for i in $disks;
 do
 	echo $i
 done
 
-echo
+echo "\n4) Процессы"
+ps -aux
+echo "Часть 2: Информация о сети"
+echo -e "\n1) Сетевые интерфейсы:\n"
+ifconfig
+echo -e "\n2) Таблица маршрутизации:\n"
+route -nve
 
-echo "4) Процессы"
-echo "$(ps -aux)"
+echo -e "\n3) Arp-таблица:\n"
+arp -en
+
+echo -e "\n4) Сетевые подключения:\n"
+netstat -ano
+
+echo -e "\nОткрытые порты и запущенные сервисы:\n"
+
+touch /tmp/ports.py
+echo -e "import socket\n" >> /tmp/ports.py
+echo "counter = 1" >> /tmp/ports.py
+echo "for port in range(1, 65536):" >> /tmp/ports.py
+echo "        try:" >> /tmp/ports.py
+echo "                sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)" >> /tmp/ports.py
+echo "                sock.connect(('127.0.0.1', port))" >> /tmp/ports.py
+echo "                service = socket.getservbyport(port, \"tcp\")" >> /tmp/ports.py
+echo "                print(f'{counter}) {str(port)}\ttcp\t{service}')" >> /tmp/ports.py
+echo "                counter += 1" >> /tmp/ports.py
+echo "                sock.close()" >> /tmp/ports.py
+echo "        except:" >> /tmp/ports.py
+echo "                pass" >> /tmp/ports.py
+
+echo -e "\nfor port in range(1, 65536):" >> /tmp/ports.py
+echo "        try:" >> /tmp/ports.py
+echo "                sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)" >> /tmp/ports.py
+echo "                sock.connect(('127.0.0.1', port))" >> /tmp/ports.py
+echo "                service = socket.getservbyport(port, \"udp\")" >> /tmp/ports.py
+echo "                print(f'{counter}) {str(port)}\tudp\t{service}')" >> /tmp/ports.py
+echo "                counter += 1" >> /tmp/ports.py
+echo "                sock.close()" >> /tmp/ports.py
+echo "        except:" >> /tmp/ports.py
+echo "                pass" >> /tmp/ports.py
+
+if [[ -e /usr/bin/python3 ]];
+then
+	python3 /tmp/ports.py
+elif [[ -e /usr/bin/python2 ]];
+then
+	python2 /tmp/ports.py
+fi
+rm -rf /tmp/ports.py
 echo
 echo "-------------------------------------------------------------------------------------------------------------------"
